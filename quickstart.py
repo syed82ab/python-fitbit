9e06d7f6f50fe83f52aed55d20e1a3ae06285a95
syed82ab <syed.aljunid@gmail.com>
2021-05-15 00:12:55 +0800

    Working script to update personal weight, fat percentage from sys.args and tokens.txt
    updating of token from refresh token not implemented yet.

--- quickstart.py
+++ quickstart.py
@@ -1,25 +1,38 @@
 import fitbit
 import gather_keys_oauth2 as Oauth2
 import datetime
+import token_access
+import sys
+
+if not (len(sys.argv) == 3):
+        print("Arguments: weight and fat percentage")
+        sys.exit(1)
+weight = sys.argv[1]
+fat = sys.argv[2]
+
 
 CLIENT_ID = '239Z3V'
 CLIENT_SECRET = '7ed20900e40156827fda3b90c9538baf'
+#Get and write the tokens from here
 
 
-server = Oauth2.OAuth2Server(CLIENT_ID, CLIENT_SECRET)
-server.browser_authorize()
-ACCESS_TOKEN = str(server.fitbit.client.session.token['access_token'])
-REFRESH_TOKEN = str(server.fitbit.client.session.token['refresh_token'])
+
+#server = Oauth2.OAuth2Server(CLIENT_ID, CLIENT_SECRET)
+#server.browser_authorize()
+#ACCESS_TOKEN = str(server.fitbit.client.session.token['access_token'])
+#REFRESH_TOKEN = str(server.fitbit.client.session.token['refresh_token'])
+ACCESS_TOKEN, REFRESH_TOKEN = token_access.GetConfig()
+
 auth2_client = fitbit.Fitbit(CLIENT_ID, CLIENT_SECRET, oauth2=True, access_token=ACCESS_TOKEN, refresh_token=REFRESH_TOKEN)
 
 yesterday = str((datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y%m%d"))
 yesterday2 = str((datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y-%m-%d"))
 today = datetime.datetime.now()
 
-weight = 60.00
-fat = 24.5
 fit_weight = auth2_client.log_weight(weight,today)
 fit_fat = auth2_client.log_fat(fat,today)
 
 #fit_statsHR = auth2_client.intraday_time_series('activities/heart', base_date=yesterday2, detail_level='1min')
 prof = auth2_client.user_profile_get()
+
+#token_access.WriteConfig(ACCESS_TOKEN,REFRESH_TOKEN)
---
+++ token_access.py
@@ -0,0 +1,53 @@
+from requests.auth import HTTPBasicAuth
+
+IniFile = "tokens.txt"
+
+def GetConfig():
+    print( "Reading from the config file")
+
+    #Open the file
+    FileObj = open(IniFile,'r')
+
+    #Read first two lines - first is the access token, second is the refresh token
+    AccToken = FileObj.readline()
+    RefToken = FileObj.readline()
+
+    #Close the file
+    FileObj.close()
+    #See if the strings have newline characters on the end.  If so, strip them
+    if (AccToken.find("\n") > 0):
+        AccToken = AccToken[:-1]
+    if (RefToken.find("\n") > 0):
+        RefToken = RefToken[:-1]
+
+    #Return values
+    return AccToken, RefToken
+
+def WriteConfig(AccToken,RefToken):
+    print("Writing new token to the config file")
+    print("Writing this: " + AccToken + " and " + RefToken)
+
+    #Delete the old config file
+    #os.remove(IniFile)
+
+    #Open and write to the file
+    FileObj = open(IniFile,'w')
+    FileObj.write(AccToken + "\n")
+    FileObj.write(RefToken + "\n")
+    FileObj.close()
+
+#Make a HTTP POST to get a new
+def GetNewAccessToken(auth2_client):
+    print( "Getting a new access token")
+
+    token = auth2_client.client.session.refresh_token(
+            auth2_client.client.refresh_token_url,
+            auth=HTTPBasicAuth(auth2_client.client.client_id, auth2_client.client.client_secret)
+            )
+    AccToken = token['access_token'];
+    RefToken = token['refresh_token'];
+   
+    #Write the access token to the ini file
+    WriteConfig(AccToken, RefToken)
+
+    return AccToken, RefToken
